0ee3b882d7874c948eb0bcc60ceae8ceea672591
Logan McNeely <logan.k.mcneely@gmail.com>
2020-10-14 16:49:21 -0500

    Added user's index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 module ApplicationHelper
+
   def gravatar_for(user, options = { size: 80 })
     hash = Digest::MD5.hexdigest(user.email)
     gravatar_url = "https://www.gravatar.com/avatar/hash/#{hash}?s=#{options[:size]}"
     image_tag(gravatar_url, alt: user.username, class: "rounded mx-auto d-block shadow")
   end
+  
 end
