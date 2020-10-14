1f814ce6c4bc2ce7ff9b71f9a58d13e07d3b4f41
Logan McNeely <logan.k.mcneely@gmail.com>
2020-10-14 10:30:03 -0500

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
-  
+  validates :username, 
+    presence: true, 
+    uniqueness: { case_sensitive: false }, 
+    length: { minimum: 3, maximum: 25 }
+  validates :email, 
+    presence: true, 
+    uniqueness: { case_sensitive: false }, 
+    length: { maximum: 105 },
+    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
 end
