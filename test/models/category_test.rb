dbf6cf92b6ba30af182c24f6aa87742f14ebd700
Logan McNeely <logan.k.mcneely@gmail.com>
2020-10-15 14:23:17 -0500

    added categories table and testing conditions

---
+++ test/models/category_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+
+  def setup
+    @category = Category.new(name: "Test")
+  end
+
+  test "category should be valid" do
+    assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    @new_category = Category.new(name: "Test")
+    assert_not @new_category.valid?
+
+  end
+
+  test "name should not be too long" do
+    @category.name = "a" * 21
+    assert_not @category.valid?
+  end
+
+  test "name shoudl not be too short" do
+    @category.name = "a" * 2
+    assert_not @category.valid?
+  end
+
+end
