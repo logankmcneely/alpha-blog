d2660ee9d25ab2f6e4e674c6f96e15fc8ffaf3d7
Logan McNeely <logan.k.mcneely@gmail.com>
2020-10-15 20:26:48 -0500

    added new and create category controllers and tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,9 +4,27 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @category = Category.find(params[:id])
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save 
+      flash[:notice] = "You have successfully created the category #{@category.name}"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
