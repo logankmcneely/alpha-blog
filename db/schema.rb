eb0cf7f5f2ad2bec24d6aa9698878b47aa856ff1
Logan McNeely <logan.k.mcneely@gmail.com>
2020-10-16 09:22:29 -0500

    ArticleCategory back end implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_184813) do
+ActiveRecord::Schema.define(version: 2020_10_16_140836) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
