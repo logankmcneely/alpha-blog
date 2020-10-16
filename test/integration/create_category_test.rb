cf2116cd3824ea2d1374e5d2c18a924cbddba220
Logan McNeely <logan.k.mcneely@gmail.com>
2020-10-16 09:04:47 -0500

    added admin reqs for create/new categories and added to navbar

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -2,7 +2,16 @@ require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
 
+  setup do
+    @admin_user = User.create(
+      username: "Admin", 
+      email: "test_admin@example.com", 
+      password: "password",
+      admin: true)
+  end
+
   test "get new category form and create category" do
+    sign_in_as(@admin_user)
     get "/categories/new"
     assert_response :success
     assert_difference('Category.count', 1) do
@@ -15,6 +24,7 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
   end
 
   test "get new category form and reject invalid category submission" do
+    sign_in_as(@admin_user)
     get "/categories/new"
     assert_response :success
     assert_no_difference 'Category.count' do
