a43b1fcc462acd6c1e72cf9fc89b1106246c79f3
Logan McNeely <logan.k.mcneely@gmail.com>
2020-11-19 15:05:17 -0600

    initial update of articles index and update of pagination styles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
   end
 
   def index
-    @articles = Article.paginate(page: params[:page], per_page: 5)
+    @articles = Article.order("created_at DESC").paginate(page: params[:page], per_page: 5)
   end
 
   def new
