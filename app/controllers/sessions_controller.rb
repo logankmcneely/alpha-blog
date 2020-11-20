d67b8868fdf05147a0f39fb9f1603bed9b745652
Logan McNeely <logan.k.mcneely@gmail.com>
2020-11-20 12:46:48 -0600

    navigation collapse and expand works with all nav links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to user
+      redirect_to articles_path
     else
       flash.now[:alert] = "Invalid login details."
       render 'new'
