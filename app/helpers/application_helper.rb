e6972a7aedbd9cdd5525efbaff4155bf59fa1c30
Logan McNeely <logan.k.mcneely@gmail.com>
2020-11-22 13:02:57 -0600

    some mobile and small screen adjustments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,13 @@
 
 module ApplicationHelper
 
-  def gravatar_for(user, options = { size: 80 })
-    hash = Digest::MD5.hexdigest(user.email)
-    gravatar_url = "https://www.gravatar.com/avatar/hash/#{hash}?s=#{options[:size]}"
-    image_tag(gravatar_url, alt: user.username, class: "rounded mx-auto d-block shadow")
+  def get_header
+    case request.path
+    when articles_path
+      "Articles"
+    else
+      "Alpha Blog"
+    end
   end
   
 end
