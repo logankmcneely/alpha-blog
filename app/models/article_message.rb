a72f21793cec80fe3d1de796da0cef41e44248d3
Logan McNeely <logan.k.mcneely@gmail.com>
2020-12-09 12:06:05 -0600

    updated documentation on models

--- app/models/article_message.rb
+++ app/models/article_message.rb
@@ -1,5 +1,7 @@
 class ArticleMessage < ApplicationRecord
   belongs_to :article
   belongs_to :user
+
+  # <ArticleMessage> uses ActionText wtih rich text to store the message body
   has_rich_text :body
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,17 @@
 class Category < ApplicationRecord
-  has_many :article_categories
-  has_many :articles, through: :article_categories
-  
+
   validates :name, 
     presence: true, 
     uniqueness: true,
     length: { minimum: 3, maximum: 20}
+
+
+  # <Category>.article_categories returns the <ArticleCategory> of associated <Category>
+  #   Ex: [<ArticleCategory_id: __, article_id: __, category_id: __>, ...]
+  has_many :article_categories
+
+  # <Category>.articles returns all <Article> associated with the <Category>
+  #   Ex: [<Article_id: __, title: __, created_at: __, ...>, ...]
+  has_many :articles, through: :article_categories
+  
 end
